7fd1c38920fd6971b9c676853877ad29ba089af6
Gemofort <vanya6677@gmail.com>
2019-07-17 23:02:07 +0300

    HW9: engines and public created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const views = require('koa-views');
 const Router = require('koa-router');
+const serve = require('koa-static');
 const path = require('path');
 
 const app = new Koa();
@@ -13,12 +14,7 @@ app.use(views(path.join(__dirname, 'src/templates'), {
   },
 }));
 
-app.use(async (ctx, next) => {
-  console.log('ping');
-  console.log(ctx.response);
-  await next();
-  console.log(ctx.response);
-});
+app.use(serve(path.join(__dirname, '/public')));
 
 router.use('/', require('./src/routes').routes());
 
